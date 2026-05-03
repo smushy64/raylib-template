9c60daade8097bd37a7457f5357e051d08eb5793
Alicia Amarilla <smushyaa@gmail.com>
2026-05-03 15:24:14 -0400

    "v6.0: restarted project, submodule raylib pinned to 6.0 added"

--- src/sources.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @file   sources.c
- * @brief  Source files for Raylib project.
- * @author Alicia Amarilla (smushyaa@gmail.com)
- * @date   October 08, 2025
-*/
-#include "../src/main.cpp"
-#include "../src/rl/entry.cpp"
-
